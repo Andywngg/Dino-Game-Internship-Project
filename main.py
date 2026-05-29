34abf29b19aa51af0b4e00f7d32ff3295355e028
Andy Wang <andywang.437@gmail.com>
2026-05-29 09:47:41 -0400

    Tutorial Game

--- main.py
+++ main.py
@@ -2,15 +2,31 @@
 
 A game similar to the famous Chrome Dino Game, built using pygame-ce.
 Made by intern: @bassemfarid, no one or nothing else. 🤖
+Additional author: Andy Wang
+Commit hash: 
 """
 
 import pygame
+from random import randint
 
 def display_score(start_time):
     current_time = int((pygame.time.get_ticks() - start_time) / 1000)
     score_surf = game_font.render(f"Score: {current_time}", False, (64, 64, 64))
     score_rect = score_surf.get_rect(center=(400, 50))
     screen.blit(score_surf, score_rect)
+    return current_time
+
+def obstacle_movement(obstacle_list):
+    if obstacle_list:
+        for obstacle_rect in obstacle_list:
+            obstacle_rect.x -= 5
+
+            screen.blit(egg_surf, obstacle_rect)
+
+        obstacle_list = [obstacle for obstacle in obstacle_list if obstacle.x > -100]
+        
+        return obstacle_list
+    else: return []
     
 # Initialize Pygame and create a window
 pygame.init()
@@ -18,23 +34,40 @@ screen = pygame.display.set_mode((800, 400))
 clock = pygame.time.Clock()
 running = True  # Pygame main loop, kills pygame when False
 score_start_time = pygame.time.get_ticks()
+score = 0
 
 # Game state variables
-is_playing = True  # Whether in game or in menu
+is_playing = False  # Whether in game or in menu
 GROUND_Y = 300  # The Y-coordinate of the ground level
 JUMP_GRAVITY_START_SPEED = -20  # The speed at which the player jumps
 players_gravity_speed = 0  # The current speed at which the player falls
+player_stand = pygame.image.load('graphics/player/player_jump.png').convert_alpha()
+player_stand = pygame.transform.rotozoom(player_stand,0,2)
+player_stand_rect = player_stand.get_rect(center = (400, 200))
+
 
 # Load level assets
 SKY_SURF = pygame.image.load("graphics/level/sky.png").convert()
 GROUND_SURF = pygame.image.load("graphics/level/ground.png").convert()
 game_font = pygame.font.Font(pygame.font.get_default_font(), 50)
+game_name = game_font.render("Brainrot Run", False, (111, 196, 169))
+game_name_rect = game_name.get_rect(center = (400,80))
+game_message = game_font.render("Press space to run", False, (111, 196, 169))
+game_message_rect = game_message.get_rect(center = (400, 320))
+
+# Timer
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer, 1600)
+
+obstacle_rect_list = []
 
 # Load sprite assets
 player_surf = pygame.image.load("graphics/player/player_walk_1.png").convert_alpha()
 player_rect = player_surf.get_rect(bottomleft=(25, GROUND_Y))
 egg_surf = pygame.image.load("graphics/egg/egg_1.png").convert_alpha()
-egg_rect = egg_surf.get_rect(bottomleft=(800, GROUND_Y))
+egg_rect = egg_surf.get_rect(bottomleft=(randint(900, 1100), GROUND_Y))
+
+bc = pygame.image.load("graphics/bombardino/bc.png").convert_alpha()
 
 
 while running:
@@ -58,6 +91,10 @@ while running:
                 is_playing = True
                 egg_rect.left = 800
                 score_start_time = pygame.time.get_ticks()
+            
+        if event.type == obstacle_timer and is_playing:
+            obstacle_rect_list.append(egg_surf.get_rect(bottomleft=(800, GROUND_Y)))
+
 
     if is_playing:
         screen.fill("purple")  # Wipe the screen
@@ -65,7 +102,7 @@ while running:
         # Blit the level assets
         screen.blit(SKY_SURF, (0, 0))
         screen.blit(GROUND_SURF, (0, GROUND_Y))
-        display_score(score_start_time)
+        score = display_score(score_start_time)
 
         # Adjust egg's horizontal location then blit it
         egg_rect.x -= 5
@@ -80,13 +117,24 @@ while running:
             player_rect.bottom = GROUND_Y
         screen.blit(player_surf, player_rect)
 
+        # Obstacle movement
+        obstacle_rect_list = obstacle_movement(obstacle_rect_list)
+
         # When player collides with enemy, game ends
         if egg_rect.colliderect(player_rect):
             is_playing = False
 
     # When game is over, display game over message
     else:
-        screen.fill("black")
+        screen.fill("white")
+        screen.blit(player_stand, player_stand_rect)
+        score_message = game_font.render(f"Your score: {score}", False, (111, 196, 169))
+        score_message_rect = score_message.get_rect(center = (400, 330))
+        screen.blit(game_name,game_name_rect)
+        if score == 0:
+            screen.blit(game_message, game_message_rect)
+        else:
+            screen.blit(score_message, score_message_rect)
 
     # flip the display to put your work on screen
     pygame.display.flip()
